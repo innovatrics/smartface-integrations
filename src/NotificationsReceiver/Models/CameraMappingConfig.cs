d76f14f87c639704f13e9a5890bfc9c3940d2d64
Luke-1988 <37071890+Luke-1988@users.noreply.github.com>
2022-05-25 11:08:56 +0800

    Feature/zero mq sample (#2)
    * WIP
    
    * WIP II.
    
    * WIP III.

---
+++ src/NotificationsReceiver/Models/CameraMappingConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Innovatrics.SmartFace.Integrations.NotificationsReceiver.Models
+{
+    public class CameraMappingConfig
+    {
+        public Guid StreamId                            { get; set; }
+        public CameraMappingConfigCamera NXCamera       { get; set; }
+    }
+
+    public class CameraMappingConfigCamera
+    {
+        public string Id                                { get; set; }
+        public string Name                              { get; set; }
+    }
+}
---
+++ src/NotificationsReceiver/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+
+using Innovatrics.SmartFace.Integrations.Shared.Logging;
+
+namespace Innovatrics.SmartFace.Integrations.NotificationsReceiver
+{
+    public class Program
+    {
+        private const string APP_NAME = "NotificationsReceiver";
+
+        public static CancellationTokenSource CancellationTokenSource = new CancellationTokenSource();
+        public static bool ReadKeyToStop = Environment.UserInteractive;
+
+        public static async Task Main(string[] args)
+        {
+            try
+            {
+                var logger =  LoggingSetup.SetupBasicLogging(APP_NAME);
+
+                var configurationRoot = new ConfigurationBuilder()
+                    .AddJsonFile("appSettings.json", optional: false)
+                    .AddEnvironmentVariables()
+                    .AddCommandLine(args)
+                    .Build();
+
+                
+                var hostBuilder = CreateHostBuilder(args, logger, configurationRoot);
+                using var host = hostBuilder.Build();
+
+                var hostApplicationLifetime = host.Services.GetRequiredService<IHostApplicationLifetime>();
+
+                var applicationTask = host.RunAsync();
+
+                await using (hostApplicationLifetime.ApplicationStarted.Register(() =>
+                {
+                    Log.Information("Application started.");
+                })) { }
+
+                await applicationTask;
+
+                Log.Information("Application stopped.");
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Application start-up failed.");
+                Log.CloseAndFlush();
+                throw;
+            }
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args, ILogger logger, IConfigurationRoot configurationRoot)
+        {
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(builder =>
+                {
+                    builder.Sources.Clear();
+                    builder.AddConfiguration(configurationRoot);
+                })
+                .ConfigureServices((context, services) =>
+                {
+                    ConfigureServices(services, logger);
+                })
+                .UseSerilog(logger)
+                .UseSystemd()
+                .UseWindowsService()
+            ;
+        }
+
+        public static void ConfigureServices(IServiceCollection services, ILogger logger)
+        {
+            services.AddHttpClient();
+   
+            services.AddHostedService<WorkerService>();
+        }
+    }
+}
