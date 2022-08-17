9d4b885355d991bb5f4bc10f957f1fd1fcfd5c37
Luke-1988 <37071890+Luke-1988@users.noreply.github.com>
2022-08-17 13:21:02 +0700

    FaceGate

---
+++ src/FaceGate/Models/CameraMappingConfig.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Innovatrics.SmartFace.Integrations.FaceGate.Models
+{
+    public class CameraMappingConfig
+    {
+        public Guid Source      { get; set; }
+        public string Target    { get; set; }
+    }
+}
