6b1fcfecc49cf565d67556de8cac79de0235e903
Lukas Malik <lukasmalik11@gmail.com>
2022-11-15 08:45:42 +0100

    build fix

--- src/RelayConnector/Connectors/IRelayConnector.cs
+++ src/RelayConnector/Connectors/IRelayConnector.cs
@@ -6,6 +6,6 @@ namespace Innovatrics.SmartFace.Integrations.RelayConnector.Connectors
     public interface IRelayConnector
     {
         Task OpenAsync(string ipAddress, int port, int channel, string username = null, string password = null);
-        Task SendKeepAliveAsync(string ipAddress, int port, int channel, string username = null, string password = null);
+        Task SendKeepAliveAsync(string ipAddress, int port, int channel = null, string username = null, string password = null);
     }
 }
