aaff06336d3c98ba945ad80b1d185b1b053f360d
Juraj <juraj.beres@innovatrics.com>
2023-01-30 13:48:02 +0100

    WIP Refactoring AeosSyncTool to use SF structure

---
+++ src/AEOSSyncTool/Adapters/AeosDataAdapter.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+
+namespace Innovatrics.SmartFace.Integrations.AEOSSync
+{
+    public class AeosDataAdapter : ISmartFaceDataAdapter
+    {
+        private readonly ILogger logger;
+        private readonly IConfiguration configuration;
+        private readonly IHttpClientFactory httpClientFactory;
+
+        public AeosDataAdapter(
+            ILogger logger,
+            IConfiguration configuration,
+            IHttpClientFactory httpClientFactory
+        )
+        {
+            this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            this.configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+            this.httpClientFactory = httpClientFactory ?? throw new ArgumentNullException(nameof(httpClientFactory));
+            this.logger.Information("AeosDataAdapter Initiated");
+        }
+
+        //public async Task OpenAsync(string checkpoint_id, string ticket_id, int chip_id = 12)
+        public async Task OpenAsync()
+        {
+            this.logger.Information("AeosDataAdapter");
+
+        }
+
+        public async Task getEmployees()
+        {
+            this.logger.Information("Receiving Employees");
+        }
+
+        public async Task createEmployees()
+        {
+            this.logger.Information("Creating Employees");
+        }
+
+        public async Task updateEmployees()
+        {
+            this.logger.Information("Updating Employees");
+        }
+
+        public async Task removeEmployees()
+        {
+            this.logger.Information("Removing Employees");
+        }
+    }
+}
