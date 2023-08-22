61ef8798619f197d17f59ae4b0de4856a5b006b2
Juraj <juraj.beres@innovatrics.com>
2023-08-22 11:11:21 +0200

    Update Fix

--- src/AEOS/AeosSync/AeosExtensions.cs
+++ src/AEOS/AeosSync/AeosExtensions.cs
@@ -144,7 +144,6 @@ namespace Innovatrics.SmartFace.Integrations.AeosSync
         {
             if (keepPhotoUpToDate)
             {
-                // update here && smartFaceMember.Note != null
                 if (aeosMember.ImageData != null && smartFaceMember.Note != null)
                 {
                     if (getImageHash(aeosMember.ImageData).SequenceEqual(smartFaceMember.Note))
