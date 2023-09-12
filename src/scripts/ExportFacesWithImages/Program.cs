b53fb211768279017d761e06f490cb5fd3e324f1
Lukas Malik <lukasmalik11@gmail.com>
2023-09-12 15:16:07 +0200

    WIP

--- src/scripts/ExportFacesWithImages/Program.cs
+++ src/scripts/ExportFacesWithImages/Program.cs
@@ -7,19 +7,14 @@ namespace Innovatrics.SmartFace.Integrations.ExportFacesWithImages
 {
     public class Program
     {
-        private const string SMARTFACE_API_URL = "http://10.11.64.18:8098";
-        private const string SMARTFACE_GRAPHQL_URL = "http://10.11.64.18:8097/graphql";
+        private const string SMARTFACE_API_URL = "http://localhost:8098";
+        private const string SMARTFACE_GRAPHQL_URL = "http://localhost:8097/graphql";
 
 
         static async Task<int> Main(string[] args)
         {
             var cameras = new string[] {
-                "1b6d42bd-431a-4bc3-a115-4008bd726dbb",
-                "19b10e5b-aebf-4c00-8ef1-24c2def86925",
-                "847f65b6-3211-41ae-8a33-72d595a48ce7",
-                "d5ff8f40-f900-4492-8ecc-6a2539648964",
-                "f9de3d6b-8f98-4bef-13bb-08db0aa598ba",
-                "6b7b8f5c-6c28-4ccb-1b9e-08db2952c2b7"
+                "2ec79185-2808-4ccc-83a4-5eff267e60df"
             }
                 .Select(s => Guid.Parse(s))
                 .ToArray()
