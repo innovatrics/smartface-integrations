caea0c634b70865f01676feddd7ef1a25d6f02e8
Juraj <juraj.beres@innovatrics.com>
2024-04-04 13:37:22 +0200

    WIP

--- src/MyQConnector/Connectors/MyQConnector.cs
+++ src/MyQConnector/Connectors/MyQConnector.cs
@@ -73,55 +73,10 @@ namespace Innovatrics.SmartFace.Integrations.MyQConnectorNamespace.Connectors
 
                 */
 
-                /*
-                var messageBytes = new byte[2 + clientId.Length + 2];
-                messageBytes[0] = 0x02;
-                messageBytes[1] = (byte)clientId.Length;
-                for (int i = 0; i < clientId.Length; i++)
-                {
-                    messageBytes[2 + i] = clientId[i];
-                }
+                
 
-                byte checksum = 0;
 
-                for (int i = 0; i < clientId.Length + 1; i++)
-                {
-                    checksum = (byte)(checksum ^ messageBytes[1 + i]);
-                }
-
-                messageBytes[messageBytes.Length - 2] = checksum;
-                messageBytes[messageBytes.Length - 1] = 0x03;
-
-                try
-                {
-                    this.logger.Debug($"Sending {messageBytes.Length} bytes to socket");
-
-                    var bytesSent = socket.Send(messageBytes);
-
-                    this.logger.Debug($"Sent {bytesSent} bytes");
-
-                    var messageReceived = new byte[1024];
-
-                    this.logger.Debug($"Reading from socket");
-
-                    var bytesReceived = socket.Receive(messageReceived);
-
-                    this.logger.Debug($"Read {bytesReceived} bytes from socket");
-
-                    this.logger.Information("Message from socket {0}", Encoding.ASCII.GetString(messageReceived, 0, bytesReceived));
-                }
-                catch (SocketException se)
-                {
-                    this.logger.Error($"SocketException: {se.Message}, Code {se.SocketErrorCode}");
-                    socket?.Shutdown(SocketShutdown.Both);
-                    socket?.Dispose();
-                    socket = null;
-                }
-                catch (Exception e)
-                {
-                    this.logger.Error("Unexpected exception : {0}", e.ToString());
-                }
-                */
+                
             }
 
             catch (Exception e)
