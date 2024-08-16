b7ee15febc30064d383994510461693e974c010d
Lukas Malik <lukasmalik11@gmail.com>
2024-08-16 11:47:36 +0200

    initial refactoring of projects

--- src/AEOS/AeosSync/Adapters/SmartFaceDataAdapter.cs
+++ src/AEOS/AeosSync/Adapters/SmartFaceDataAdapter.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using Serilog;
-using Innovatrics.SmartFace.Integrations.AEOS.SmartFaceClients;
+using Innovatrics.SmartFace.Integrations.Shared.SmartFaceRestApiClient;
 using Innovatrics.SmartFace.Integrations.AEOS.SmartFaceClients;
 using System.Collections.Generic;
 using System.Linq;
