bd238aa393c31cf8aa83588ddef5b9e10435b2d3
Lukas Malik <lukasmalik11@gmail.com>
2025-07-03 14:56:04 +0200

    TComServerConnector VIII

--- src/AccessControlConnector/Connectors/Cominfo/ITServerClient.cs
+++ src/AccessControlConnector/Connectors/Cominfo/ITServerClient.cs
@@ -3,6 +3,12 @@ using System.Net;
 
 namespace Innovatrics.SmartFace.Integrations.AccessControlConnector.Connectors.Cominfo
 {
+    public delegate void ConnectionStateChange(bool connected);
+    public delegate void DeviceListCallback(PrtclCmfJson.MsgDevlistResp devList);
+    public delegate void DevEventCallback(PrtclCmfJson.MsgDevEvent devEvent);
+    public delegate void ActionRespCallback(PrtclCmfJson.MsgActionResp actionResp);
+
+
     public interface ITServerClient
     {
         bool IsConnected { get; }
@@ -11,4 +17,4 @@ namespace Innovatrics.SmartFace.Integrations.AccessControlConnector.Connectors.C
         bool SendMessage(PrtclCmfJson.Header message);
         void Close();
     }
-} 
+}
