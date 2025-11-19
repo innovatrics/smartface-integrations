979891e21afc2b6b61d2b908a5f833d24448d323
Ivan Benovic <ivan.benovic@innovatrics.com>
2025-11-19 12:33:00 +0100

    Remove unused ctor params

--- src/AccessControlConnector/Service/AccessControlConnectorService.cs
+++ src/AccessControlConnector/Service/AccessControlConnectorService.cs
@@ -1,6 +1,4 @@
 using System;
-using System.IO;
-using System.Net.Http;
 using System.Threading.Tasks;
 using System.Threading.Tasks.Dataflow;
 using Innovatrics.SmartFace.Integrations.AccessController.Notifications;
@@ -13,21 +11,15 @@ namespace Innovatrics.SmartFace.Integrations.AccessControlConnector.Services
     {
         public readonly int MaxParallelBlocks;
         private readonly ILogger _logger;
-        private readonly IConfiguration _configuration;
-        private readonly IHttpClientFactory _httpClientFactory;
         private readonly IBridgeService _bridgeService;
         private ActionBlock<GrantedNotification> _actionBlock;
 
         public AccessControlConnectorService(
             ILogger logger,
             IConfiguration configuration,
-            IHttpClientFactory httpClientFactory,
-            IBridgeService bridgeService
-        )
+            IBridgeService bridgeService)
         {
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
-            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
-            _httpClientFactory = httpClientFactory ?? throw new ArgumentNullException(nameof(httpClientFactory));
             _bridgeService = bridgeService ?? throw new ArgumentNullException(nameof(bridgeService));
 
             MaxParallelBlocks = configuration.GetValue<int>("Config:MaxParallelActionBlocks", 4);
